dcbbf8c488937194114d696a6c3f03c78e6bd6f7
YaninYing18 <47548231+YaninYing18@users.noreply.github.com>
2019-02-12 12:10:51 +0700

    Add files via upload

---
+++ nav.php
@@ -0,0 +1,15 @@
+ <li class="nav-item">
+          <a class="nav-link active" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="newbooks.html">new Books</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="search.html">search Book</a>
+        </li>
+		 <li class="nav-item">
+          <a class="nav-link" href="display_books.php">Display Book</a>
+        </li>
+		 <li class="nav-item">
+          <a class="nav-link" href="page-5.php">Page 5</a>
+        </li>
